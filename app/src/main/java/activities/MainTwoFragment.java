87550bd233acc822c0c69d7b95197962fb93f85b
SpringSmell <894919296@qq.com>
2015-08-20 23:11:55 +0800

    The music pavilion
    Part of the

--- app/src/main/java/activities/MainTwoFragment.java
+++ app/src/main/java/activities/MainTwoFragment.java
@@ -3,78 +3,140 @@ package activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.activities.zoulx.homeactivity.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import Bean.TabInfo;
 import adapter.WidgetAdapter;
 import manage.Constant;
 import widgets.TitleIndicator;
-import widgets.ViewPagerCompat;
 
 public class MainTwoFragment extends Fragment implements ViewPager.OnPageChangeListener {
 
     private View mView;
-    private ViewPagerCompat mViewPagerCompat;
+    private ViewPager mViewPager;
     private TitleIndicator mTitleIndicator;
     private WidgetAdapter mWidgetAdapter;
     private ArrayList<TabInfo> mTabs;
-    /**当前显示的页面*/
-    private int mCurrentTab;
+    /**
+     * 当前显示的页面
+     */
+    private int mCurrentTab = 0;
+    private int mLastTab = -1;
     protected Context mContext;
 
     public MainTwoFragment() {
         super();
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.musics_pavilion, null);
-//        init();
+        init();
         return mView;
     }
 
     private void init() {
-        mTabs= new ArrayList<>();
-        mWidgetAdapter=new WidgetAdapter(getActivity(),getChildFragmentManager(),mTabs);
-        mViewPagerCompat = (ViewPagerCompat) mView.findViewById(R.id.musics_pavilion_pager);
+
+        mCurrentTab = supplyTabs();
+        mWidgetAdapter = new WidgetAdapter(getActivity(), getChildFragmentManager(), mTabs);
+        mViewPager = (ViewPager) mView.findViewById(R.id.musics_pavilion_pager);
         mTitleIndicator = (TitleIndicator) mView.findViewById(R.id.musics_pavilion_pager_indicator);
-        mCurrentTab=0;
-        Intent data=getActivity().getIntent();
-        if(null!=data){
-            mCurrentTab=data.getIntExtra(Constant.EXTRA_TAB,0);
+
+        Intent data = getActivity().getIntent();
+        if (null != data) {
+            mCurrentTab = data.getIntExtra(Constant.EXTRA_TAB, 0);
         }
-        mViewPagerCompat.setAdapter(mWidgetAdapter);
-        mViewPagerCompat.setOnPageChangeListener(this);
-        mViewPagerCompat.setOffscreenPageLimit(mTabs.size());
-        mTitleIndicator.init(mCurrentTab,mTabs,mViewPagerCompat);
+        mViewPager.setAdapter(mWidgetAdapter);
+        mViewPager.setOnPageChangeListener(this);
+        mViewPager.setOffscreenPageLimit(mTabs.size());
+        mTitleIndicator.init(mCurrentTab, mTabs, mViewPager);
+        mViewPager.setCurrentItem(mCurrentTab);
+        mLastTab = mCurrentTab;
     }
 
+    /**
+     * 添加一个标签
+     */
+    public void addTabInfo(TabInfo tab) {
+        mTabs.add(tab);
+        mWidgetAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * 从列表添加选项卡
+     *
+     * @param tabs
+     */
+    public void addTabInfos(ArrayList<TabInfo> tabs) {
+        mTabs.addAll(tabs);
+        mWidgetAdapter.notifyDataSetChanged();
+    }
+
+
+    private int supplyTabs() {
+        mTabs = new ArrayList<>();
+        mTabs.add(new TabInfo(1, getString(R.string.recommend), MusicPavilionOneFragment.class));
+        mTabs.add(new TabInfo(2, getString(R.string.products), MusicPavilionTwoFragment.class));
+        mTabs.add(new TabInfo(3, getString(R.string.song_menu), MusicPavilionThreeFragment.class));
+        mTabs.add(new TabInfo(4, getString(R.string.station), MusicPavilionThreeFragment.class));
+        mTabs.add(new TabInfo(5, getString(R.string.MV), MusicPavilionThreeFragment.class));
+        return 0;
+    }
 
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+        mTitleIndicator.onScrolled((mViewPager.getWidth() + mViewPager.getPageMargin()) * position + positionOffsetPixels);
     }
 
     @Override
     public void onPageSelected(int position) {
-
+        mTitleIndicator.onSwitched(position);
+        mCurrentTab = position;
     }
 
     @Override
     public void onPageScrollStateChanged(int state) {
+        if (state == ViewPager.SCROLL_STATE_IDLE) {
+            mLastTab = mCurrentTab;
+        }
+    }
 
+    private TabInfo getFragmentID(int tabID) {
+        if (mTabs == null) {
+            return null;
+        }
+        for (int index = 0, count = mTabs.size(); index < count; index++) {
+            TabInfo tabInfo = mTabs.get(index);
+            if (tabID == tabInfo.getId()) {
+                return tabInfo;
+            }
+        }
+        return null;
+    }
+
+    /**跳转到指定ID页面*/
+    private void navigate(int tabID) {
+        if (mTabs == null) {
+            for (int index = 0, count = mTabs.size();index < count; index++) {
+                if(mTabs.get(index).getId()==tabID){
+                    mViewPager.setCurrentItem(tabID);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString("WORKAROUND_FOR_BUG_19917_KEY", "WORKAROUND_FOR_BUG_19917_VALUE");
+        super.onSaveInstanceState(outState);
     }
 }
